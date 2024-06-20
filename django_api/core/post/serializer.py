7ff502166918a8fe1651ea9407dcd62c6c7afa69
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2024-06-20 12:07:19 +0200

    added get_liked function to post serializer.py

--- django_api/core/post/serializer.py
+++ django_api/core/post/serializer.py
@@ -33,9 +33,14 @@ class PostSerializer(AbstractSerializer):
     
     def get_likes_count(self, instance): 
         return instance.liked_by.count()
-        
+    
+    def get_liked(self, instance):
+        request = self.context.get('request', None)
+        if request is None or request.user.is_anonymous: 
+            return False
+        return request.user.has_liked(instance)
+    
     class Meta: 
         model = Post
         fields=['id', 'author', 'body', 'edited', 'created', 'updated', 'liked', 'likes_count']
         read_only_fields= ['edited'] 
-        
