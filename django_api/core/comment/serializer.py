8f23f5410b5683f1d08633e4407101ae50ebcd81
jeremiah.1582@googlemail.com <jeremiah.1582@googlemail.com>
2024-06-27 01:39:56 +0200

    starting to create CommentViewset

--- django_api/core/comment/serializer.py
+++ django_api/core/comment/serializer.py
@@ -10,6 +10,23 @@ class CommentSerializer(AbstractSerializer):
     author = SlugRelatedField(queryset=User.objects.all(), slug_field='public_id')
     post = SlugRelatedField(queryset=Post.objects.all(), slug_field='public_id')
     
+    def validate_author(self, value): 
+        if self.context["request"].user != value:
+            raise ValidationError('you cant create a post for another user')
+        return value
+    
+    def validate_post(self, value): 
+        if self.instance: 
+            return self.instance.post
+        return value
+    
+    def to_representation(self, instance):
+        rep = super().to_representation(instance) # Returnse like object 
+        author= User.objects.get_object_by_id(rep['author'])
+        rep['author'] = UserSerializer(author).data
+        
+        return rep 
+        
     class Meta:
         model = Comment
         fields= '__all__'
